d0a9e8367d4d3bfd1bc8d423120986579f37f766
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-02-01 20:05:09 -0600

    Rig Model and Migrations

---
+++ server/src/db/migrations/20210201215646-create-rig.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Rig", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Rig");
+  },
+};
