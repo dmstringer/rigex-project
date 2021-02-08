3946ed0f27d631889e16d572d702e050f459236d
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-02-08 12:22:46 -0600

    bugfix/AP5-57

--- rigex-front-end/src/components/navBar/NavBar.js
+++ rigex-front-end/src/components/navBar/NavBar.js
@@ -1,33 +1,30 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 
 import routePaths from '../../constants/routePaths';
 import logo from "../../assets/rigex-logo-small.svg";
-import search from "../../assets/search.svg";
-import account from "../../assets/account.svg";
 import "./navBar.scss"
+import history from "../../utils/history" 
 
 export default function NavBar() {
 
   return (
     <div className="navBar">
-      <AppBar position="sticky" color="inherit">
-        <Toolbar>
-          <Button href={routePaths.landing} color="inherit" >
-            <img src={logo} alt="Rigex" />
-          </Button>
-          <div className="nav-icon-box">
-            <IconButton aria-label="search bar button" >
-              <img src={search} alt="search" className="nav-icon"/>
-            </IconButton>
-            <IconButton aria-label="account of current user" >
-              <img src={account} alt="account" className="nav-icon" />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <div className="logo">
+        <button
+        onClick={() => history.push(routePaths.landing)}
+        >
+          <img src={logo} alt="Rigex"/>
+        </button >
+      </div>
+      <div className="iconBox">
+        <button aria-label="search bar button">
+          <SearchIcon fontSize="large"/>
+        </button>
+        <button aria-label="account of current user">
+          <PersonOutlineOutlinedIcon fontSize="large"/>
+        </button>
+      </div>
     </div>
   );
 }
