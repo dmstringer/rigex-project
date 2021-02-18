da9df1dc81622cc1e79744d98624b0eba4e7520f
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-02-18 15:11:00 -0600

    feature/AP5-94 Vital-Resources Setup and Header

--- rigex-front-end/src/App.js
+++ rigex-front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import routePaths from './constants/routePaths';
-import { Landing, Login, SignUp } from './pages';
+import { Landing, Login, SignUp, VitalResources } from './pages';
 export default class App extends Component {
   render() {
     return (
@@ -10,6 +10,7 @@ export default class App extends Component {
         <Route path={routePaths.landing} component={Landing} />
         <Route exact path={routePaths.login} component={Login} />
         <Route exact path={routePaths.signUp} component={SignUp} />
+        <Route exact path={routePaths.vital} component={VitalResources} />
         <Redirect from="*" to={routePaths.landing} />
       </Switch>
     );
---
+++ rigex-front-end/src/components/vital-resources/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import faker from 'faker'
+
+import './headerStyle.scss'
+
+const VitalHeader = () => {
+
+  const backgroundImg = faker.image.city()
+
+  return (
+    <div className="vital-header">
+        <img src={backgroundImg} />
+        <div className="header-text">
+            <span>The Canadian Expert Company in</span>
+            <p>Fuel Tank & Line Integrity Testing</p>
+            <span>and Zero Man Zero Risk Tank Cleaning</span>
+            <button>BRINGING THE BEST TECHNOLOGY TO MONTREAL ON 2021</button>
+        </div>
+    </div>
+  )
+}
+
+export default VitalHeader
