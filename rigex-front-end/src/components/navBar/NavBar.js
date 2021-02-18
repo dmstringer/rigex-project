0fa3c80f3c6dadb6eb5e17ca40e6bb9240b374ef
McRae Peavy <mcraepv@gmail.com>
2021-02-18 14:29:55 -0600

    bugfix/navbar-logo-link

--- rigex-front-end/src/components/navBar/NavBar.js
+++ rigex-front-end/src/components/navBar/NavBar.js
@@ -2,27 +2,24 @@ import SearchIcon from '@material-ui/icons/Search';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 
 import routePaths from '../../constants/routePaths';
-import logo from "../../assets/rigex-logo-small.svg";
-import "./navBar.scss"
-import history from "../../utils/history" 
+import logo from '../../assets/rigex-logo-small.svg';
+import './navBar.scss';
+import history from '../../utils/history';
 
 export default function NavBar() {
-
   return (
     <div className="navBar">
       <div className="logo">
-        <button
-        onClick={() => history.push(routePaths.landing)}
-        >
-          <img src={logo} alt="Rigex"/>
-        </button >
+        <button onClick={() => history.push(routePaths.landing)}>
+          <img src={logo} alt="Rigex" />
+        </button>
       </div>
       <div className="iconBox">
         <button aria-label="search bar button">
-          <SearchIcon fontSize="large"/>
+          <SearchIcon fontSize="large" />
         </button>
         <button aria-label="account of current user">
-          <PersonOutlineOutlinedIcon fontSize="large"/>
+          <PersonOutlineOutlinedIcon fontSize="large" />
         </button>
       </div>
     </div>
