1c964bed2b8c99fec147807f6569c02607288d0a
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-02-22 16:20:16 -0600

    feature/AP5-95 AboutUs

--- rigex-front-end/src/components/vital-resources/AboutUs/AboutUs.js
+++ rigex-front-end/src/components/vital-resources/AboutUs/AboutUs.js
@@ -8,29 +8,31 @@ const AboutUs = ({ content }) => {
 
   const randomImage = faker.image.image()
 
+  const aboutUsContent = {
+    listItems: []
+  }
+
+  content.forEach( contentObject => {
+    switch (contentObject.type) {
+      case 'list item':
+        aboutUsContent.listItems.push( <li> <div><CheckCircleOutlineIcon fontSize="small" /></div> {contentObject.text} </li> )
+        break;
+      default:
+        aboutUsContent[contentObject.type] = contentObject.text
+        return;
+    }
+  })
+
   return (
     <div className="about-section">
       <div className="image-container">
-        <img src={randomImage} />
+        <img src={randomImage} alt="fake image"/>
       </div>
       <div className="text-container">
-        <span className="title">About Us</span>
-        <span className="name">OILDROP GLOBAL Technology Suite</span>
-        <span className="paragraph">
-          Using a latest technology leader in a gloabl market in a prescion testing, wetsock managment and fuel restoration technologies. Our expertise enables customers to achieve the tighest storage systems, lowest fuel losses and most accurate wetstock management. Customers also beenfit from optimal pump speed, more precise re-strapping of guages, the highest compliance and cleanest fuel.
-        </span>
-        <span className="bulletpoint" >
-          <div><CheckCircleOutlineIcon fontSize="small" /></div>
-          Tank cleaning & fuel restoration
-        </span>
-        <span className="bulletpoint" >
-          <div><CheckCircleOutlineIcon fontSize="small" /></div>
-          Tank & line testing
-        </span>
-        <span className="bulletpoint" >
-          <div><CheckCircleOutlineIcon fontSize="small" /></div>
-          Wetstock management
-        </span>
+        <span className="title">{aboutUsContent.title}</span>
+        <span className="subtitle">{aboutUsContent.subtitle}</span>
+        <span className="description">{aboutUsContent.description}</span>
+        <ul className="bulletpoints" > {aboutUsContent.listItems} </ul>
       </div>
     </div>
   )
